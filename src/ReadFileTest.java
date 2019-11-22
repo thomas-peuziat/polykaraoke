67d753514cee6f475b7f9e0232ab995589c3f8ad
Mamadou Koly BOIRO <mamadou.boiro1@etu.univ-nantes.fr>
2019-11-22 15:21:50 +0100

    Creation bytecode cote serveur + Recuperation midi/titre/paroles cote client

--- src/ReadFileTest.java
+++ src/ReadFileTest.java
@@ -1,27 +1,88 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
-/**
- *
- * @author Laurent
- */
+
+
 public class ReadFileTest {
     public static void main(String[] args) {
-        readFile("/Users/mac/polykaraoke/src/myfile.txt");
+        String chemin = "files/";
+
+        long tailleMidi=0; // Nombre de bytes du fichier midi
+
+        byte[] bytesMidi = null; //Tableau contenant les bytes correspondants au fichier midi
+        byte[] bytesTxt = null; //Tableau contenant les bytes correspondants au fichier texte
+        byte[] bytesTotal; // Tableau contenant tous les bytes
+
+
+        Scanner s = null;
+
+        PrintWriter fluxSortieParoles = null;
+        String titre;
+
+        // Creation d'un fichier (en bytes) dans lequel seront stockes le midi et le texte
+        try {
+            //Ouverture d'un flux d'ecriture
+            FileOutputStream dest =new FileOutputStream(chemin + "total");
+            //Copie des bytes des fichiers midi et texte dans le nouveau fichier
+            tailleMidi = Files.copy(Paths.get(chemin + "medley.mid"), dest);
+            Files.copy(Paths.get(chemin + "texte.txt"), dest);
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichier non trouve");
+        } catch (IOException e) {
+            System.out.println("Fichier non trouve");
+        }
+
+        // Recuperation des bytes correspondants au midi et au texte et
+        try {
+            // Recuperation de tous les bytes dans un tableau
+            bytesTotal = Files.readAllBytes(new File(chemin+"total").toPath());
+            bytesMidi = new byte[(int) tailleMidi];
+
+            //Copie des bytes correspondants au fichier midi
+            for (int i=0; i < bytesMidi.length; i++) {
+                bytesMidi[i] = bytesTotal[i];
+            }
+            //Copie des bytes restants
+            bytesTxt = new byte[bytesTotal.length - (int)tailleMidi];
+            for (int j=0; j < bytesTxt.length; j++) {
+                bytesTxt[j] = bytesTotal[j+(int)tailleMidi];
+            }
+
+            //Creation des fichiers midi et texte correspondants
+            OutputStream midi = new FileOutputStream(new File (chemin+"midi.mid"));
+            OutputStream txt = new FileOutputStream(new File (chemin+"texteSortie.txt"));
+            // Ecriture des bytes dans chaque fichier
+            midi.write(bytesMidi);
+            txt.write(bytesTxt);
+
+            // Fermeture des flux
+            midi.close();
+            txt.close();
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+
+
+        // Parsing du fichier texte pour obtenir le titre et les paroles
+        try {
+            // Scanneur avec un delimiteur
+            s = new Scanner(new File(chemin+"texteSortie.txt")).useDelimiter("\\n*/////\\n*");
+            // Creation du fichier pour les paroles
+            fluxSortieParoles = new PrintWriter(new FileOutputStream(chemin+"paroles.txt"));
+            //Recup titre
+            titre = s.next();
+            // Recup paroles
+            fluxSortieParoles.print(s.next());
+            System.out.println("Titre : "+titre);
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichier non trouve");
+        } finally {
+            s.close();
+            fluxSortieParoles.close();
+        }
+
     }
 
-    public static void readFile(String pathToFile){
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(new File(pathToFile)));
-            String ligne;
-            while((ligne = reader.readLine()) != null){
-                if(ligne.startsWith("Tel :")){
-                    System.out.println(ligne);
-                }
-            }
-        } catch (Exception ex){
-            System.err.println("Error. "+ex.getMessage());
-        }
-    }
 }
